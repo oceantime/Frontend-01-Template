79018a020463755a5de95953adc5f236744e56b2
oceantime <27575910@qq.com>
2020-06-03 22:38:45 +0800

    week08 assignment

--- week07/toy-brower/parser.js
+++ week07/toy-brower/parser.js
@@ -5,7 +5,7 @@ const EOF = Symbol("EOF");
 let currentToken = null;
 let currentAttribute = null;
 let currentTextNode = null;
-let stack = [{type: "document", children: []}];
+let stack = [{ type: "document", children: [] }];
 
 let rules = [];
 
@@ -15,33 +15,307 @@ function addCSSRules(text) {
   // console.log(rules)
 }
 
-function match(element, selector) {
-  if (!selector || !element.attributes) {
+// function match(element, selector) {
+//   if (!selector || !element.attributes) {
+//     return false;
+//   }
+
+//   if (selector.charAt(0) === "#") {
+//     let attr = element.attributes.filter(attr => attr.name === "id")[0];
+//     if (attr && attr.value === selector.replace("#", "")) {
+//       return true;
+//     }
+//   } else if (selector.charAt(0) === ".") {
+//     let attr = element.attributes.filter(attr => attr.name === "class")[0];
+//     if (attr && attr.value === selector.replace(".", "")) {
+//       return true;
+//     }
+//   } else {
+//     if (element.tagName === selector) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+function match(selector, element) {
+  // console.dir(element);
+  if (!selector || !element)
     return false;
+
+  // 1. 处理非空格combinator 其他元素左右侧的空格，e.g: div a > b => div a>b
+  selector = selector.toString().trim().replace(/\s*([()>~+=^$|])\s*/g, "$1");
+  selector = selector.replace(/\s*(\*=)\s*/g, "$1");
+  // console.log(selector);
+
+  // 2. 按空格combinator分割，反转
+  const selectorParts = selector.split(' ').reverse();
+
+  // 3. 获取第一段选择器与当前元素匹配
+  const result = childAndSiblingMatch(selectorParts.shift(), element);
+  if (!result.matched)
+    return false;
+  element = result.element;
+
+  // 4. 前序空格隔断的选择器与(祖先)元素匹配
+  for (const part of selectorParts) {
+    let parentElement = element.parentElement;
+    let ancestorMatched = false;
+    while (parentElement !== null) {
+      const result = childAndSiblingMatch(part, parentElement);
+      if (result.matched) {
+        element = result.element;
+        ancestorMatched = true;
+        break;
+      }
+      parentElement = parentElement.parentElement;
+    }
+
+    if (!ancestorMatched) return false;
   }
 
-  if (selector.charAt(0) === "#") {
-    let attr = element.attributes.filter(attr => attr.name === "id")[0];
-    if (attr && attr.value === selector.replace("#", "")) {
-      return true;
-    }
-  } else if (selector.charAt(0) === ".") {
-    let attr = element.attributes.filter(attr => attr.name === "class")[0];
-    if (attr && attr.value === selector.replace(".", "")) {
-      return true;
-    }
-  } else {
-    if (element.tagName === selector) {
-      return true;
+  return true;
+}
+
+// Handle Child and Sibling combinators
+function childAndSiblingMatch(part, element) {
+  let result = {
+    matched: false,
+    element
+  }
+  // Seperate by Child combinators or Sibling combinators
+  let matches = part.split(/([>+~](?!=|\d))/g).reverse();
+  console.log(matches);
+
+  if (!simpleSelectorMatch(matches[0], element)) {
+    return result;
+  }
+
+  for (let i = 1; i < matches.length; i++) {
+    if (i % 2 === 1) {
+      // combinator
+      const combinator = matches[i];
+      if (combinator === '+') {
+        // console.log('combinator: ' + matches[i] + ', 查看前一个兄弟节点');
+        const preSibling = element.previousElementSibling;
+        const preSiblingMatch = simpleSelectorMatch(matches[i + 1], preSibling);
+
+        if (!preSiblingMatch) return result;
+        // console.log('combinator: ' + matches[i] + ', 上一个兄弟节点匹配上了');
+      } else if (combinator === '~') {
+        // console.log('combinator: ' + matches[i] + ', 遍历前面兄弟节点');
+        let preSibling = element.previousElementSibling;
+        let preSiblingMatch = false;
+
+        while (preSibling !== null) {
+          preSiblingMatch = simpleSelectorMatch(matches[i + 1], preSibling);
+          if (preSiblingMatch) {
+            break;
+          }
+          preSibling = preSibling.previousElementSibling;
+        }
+
+        if (!preSiblingMatch) return result;
+
+        // console.log('combinator: ' + matches[i] + ', 前面兄弟节点匹配上了');
+      } else if (combinator === '>') {
+        // console.log('combinator: ' + matches[i] + ', 查看父节点，如果满足要设置element 为parent');
+        const parent = element.parentElement;
+        const parentMatch = simpleSelectorMatch(matches[i + 1], parent);
+        if (!parentMatch) return result;
+
+        // console.log('combinator: ' + matches[i] + ', 父节点匹配上了');
+        element = parent;
+
+        // 设置匹配位置为父元素
+        result.element = parent;
+      }
+    } else {
+      // console.log('simple selector: ' + matches[i]);
     }
   }
-  return false;
+
+  result.matched = true;
+  return result;
+}
+
+// 简单选择器匹配
+function simpleSelectorMatch(selector, element) {
+  // console.log('simple selector = ' + selector);
+  let tag = selector.match(/^[\w-]+/);
+  let id = selector.match(/#([\w-]+)/);
+  let cls = selector.match(/\.([\w-]+)/g);
+  let props = selector.match(/\[([\w-]+([*^$~|]?=[\w\"\'-]+)?)\]/g);
+  let pseudoCls = selector.match(/:[\w-+()]+/g);
+  // console.log(tag, ids, cls, props, pseudoCls);
+
+  if (tag) {
+    if (element.tagName && element.tagName.toLowerCase() !== tag[0].toLowerCase())
+      return false;
+    // console.log('tagName matched = ' + tag[0]);
+  }
+
+  if (id) {
+    if (id[1] !== element.id) return false;
+    // console.log('element id matched = ' + element.id);
+  }
+
+  if (cls) {
+    // 去掉前边的.
+    cls = cls.map(v => v.slice(1));
+
+    for (const c of cls) {
+      if (!element.className.includes(c)) return false;
+    }
+    // console.log('element class matched = ' + cls);
+  }
+
+  if (props) {
+    // console.log(props);
+    // console.log(element);
+
+    for (let prop of props) {
+      prop = prop.replace(/\[?\]?/g, "");
+      let [key, combinator, val] = prop.split(/([*^$|~]?=)/);
+      if (val)
+        val = val.replace(/[\"]*[\']*/g, "");
+
+      // console.log(key);
+      // console.log(combinator);
+      // console.log(val);
+
+      if (!combinator) {
+        // 包含属性即可
+        if (!element.hasAttribute(key)) return false;
+        // console.log(`element prop matched key = ${key}`);
+      } else {
+        const eleVal = element.getAttribute(key);
+        switch (combinator) {
+          case '=':
+            if (eleVal !== val) return false;
+            // console.log(`= element prop matched key = ${key}`);
+            break;
+          case '*=':
+            if (!eleVal.includes(val)) return false;
+            // console.log(`*= element prop matched key = ${key}`);
+            break;
+          case '^=':
+            if (!eleVal.startsWith(val)) return false;
+            // console.log(`^= element prop matched key = ${key}`);
+            break;
+          case '$=':
+            if (!eleVal.endsWith(val)) return false;
+            // console.log(`$= element prop matched key = ${key}`);
+            break;
+          case '~=':
+            if (!eleVal.split(' ').includes(val)) return false;
+            // console.log(`~= element prop matched key = ${key}`);
+            break;
+          case '|=':
+            if (eleVal.split('-')[0] !== val) return false;
+            // console.log(`|= element prop matched key = ${key}`);
+            break;
+        }
+      }
+    }
+  }
+
+  if (pseudoCls) {
+    for (const pseudo of pseudoCls) {
+      if (pseudo === ':first-child') {
+        if (element.previousElementSibling !== null) return false;
+      } else if (pseudo === ':first-of-type') {
+        let preSibling = element.previousElementSibling;
+
+        while (preSibling !== null) {
+          if (preSibling.tagName === element.tagName) return false;
+          preSibling = preSibling.previousElementSibling;
+        }
+      } else if (pseudo === ':last-child') {
+        if (element.nextElementSibling !== null) return false;
+      } else if (pseudo === ':last-of-type') {
+        let nextSibling = element.nextElementSibling;
+
+        while (nextSibling !== null) {
+          if (nextSibling.tagName === element.tagName) return false;
+          preSibling = preSibling.nextElementSibling;
+        }
+      } else if (pseudo === ':only-child') {
+        if (element.previousElementSibling !== null || element.nextElementSibling !== null) return false;
+      } else if (pseudo.includes(':nth-child')) {
+        pseudo.match(/:nth-child\(([\d\D]+)\)/);
+        // console.log(pseudo, RegExp.$1);
+
+        const expression = RegExp.$1;
+        const index = [...element.parentElement.children].indexOf(element) + 1;
+
+        // <nth> = <an-plus-b> | even | odd
+        if (expression === 'even') {
+          if (index % 2 !== 0) return false;
+        } else if (expression === 'odd') {
+          if (index % 2 === 0) return false;
+        } else {
+          const re = expression.match(/(-?[\d]*n)?([+-])?(\d*)?/);
+          // console.log(RegExp.$1, RegExp.$2, RegExp.$3);
+          let a;
+          if (RegExp.$1) {
+            // -n -2n n 2n
+            const coefficient = RegExp.$1.replace('n', '');
+            if (coefficient === '-')
+              a = -1;
+            else if (coefficient === '')
+              a = 1;
+            else
+              a = Number(coefficient);
+          } else {
+            a = 0;
+          }
+
+          let b;
+          if (RegExp.$2) {
+            b = Number((RegExp.$2 === '+' ? RegExp.$3 : -RegExp.$3));
+          } else {
+            b = Number(RegExp.$3 || 0);
+          }
+
+          console.log(a, b, index);
+          console.log((index - b) / a, (index - b) % a);
+          if (a === 0) {
+            if (b !== index) return false;
+          } else {
+            // if ((index - b) / a >= 0 && (index - b) % a === 0) Matched
+            if ((index - b) / a < 0 || (index - b) % a !== 0) {
+              return false;
+            }
+          }
+
+          console.log(re);
+        }
+
+        // console.log(expression, index);
+      } else if (pseudo.includes(':nth-of-type')) {
+        pseudo.match(/:nth-of-type\(([\d\D]+)\)/);
+        // console.log(pseudo, RegExp.$1);
+
+        // const expression = RegExp.$1;
+        // const index = [...element.parentElement.children].indexOf(element) + 1;
+      } else if (pseudo.includes(':nth-last-child')) {
+
+      } else if (pseudo.includes(':nth-last-of-type')) {
+
+      } else if (pseudo.includes(':not')) {
+
+      }
+    }
+  }
+
+  return true;
 }
 
 function specificity(selector) {
   let p = [0, 0, 0, 0];
   let selectorParts = selector.split(" ");
-  for(let part of selectorParts) {
+  for (let part of selectorParts) {
     if (part.charAt(0) === "#") {
       p[1] += 1;
     } else if (part.charAt(0) === ".") {
@@ -54,7 +328,7 @@ function specificity(selector) {
 }
 
 function compare(sp1, sp2) {
-  if (sp1[0] -sp2[0]) {
+  if (sp1[0] - sp2[0]) {
     return sp1[0] - sp2[0];
   }
   if (sp1[1] - sp2[1]) {
@@ -72,7 +346,7 @@ function computeCSS(element) {
     element.computedStyle = {};
   }
 
-  for(let rule of rules) {
+  for (let rule of rules) {
     let selectorParts = rule.selectors[0].split(" ").reverse();
     if (!match(element, selectorParts[0])) {
       continue;
@@ -80,7 +354,7 @@ function computeCSS(element) {
 
     let matched = false;
     let j = 1;
-    for (let i = 0; i< elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
       if (match(elements[i], selectorParts[j])) {
         j++;
       }
@@ -94,7 +368,7 @@ function computeCSS(element) {
       let sp = specificity(rule.selectors[0]);
       let computedStyle = element.computedStyle;
       for (let declaration of rule.declarations) {
-        
+
         if (!computedStyle[declaration.property]) {
           computedStyle[declaration.property] = {}
         }
@@ -122,7 +396,7 @@ function emit(token) {
     };
 
     element.tagName = token.tagName;
-    
+
     for (let p in token) {
       if (p !== "type" && p !== "tagName") {
         element.attributes.push({
@@ -173,7 +447,7 @@ function data(c) {
     emit({
       type: "EOF"
     })
-    return ;
+    return;
   } else {
     emit({
       type: "text",
@@ -267,7 +541,7 @@ function beforeAttributeName(c) {
 }
 
 function beforeAttributeValue(c) {
-  if　(c.match(/^[\t\n\f ]$/) || c === "/" || c === ">" || c === EOF) {
+  if (c.match(/^[\t\n\f ]$/) || c === "/" || c === ">" || c === EOF) {
     return beforeAttributeValue;
   } else if (c === "\"") {
     return doubleQuotedAttributeValue;
@@ -308,7 +582,7 @@ function selfClosingStartTag(c) {
     currentToken.isSelfClosing = true;
     emit(currentToken);
     return data;
-  } else if (c === EOF){
+  } else if (c === EOF) {
 
   } else {
 
@@ -332,7 +606,7 @@ function doubleQuotedAttributeValue(c) {
 function singleQuotedAttributeValue(c) {
   if (c === "'") {
     currentToken[currentAttribute.name] = currentAttribute.value;
-    return afterQuotedAttributeValue;    
+    return afterQuotedAttributeValue;
   } else if (c === "\u0000") {
 
   } else if (c === EOF) {
@@ -385,7 +659,7 @@ function UnquotedAttributeValue(c) {
 
 module.exports.parseHTML = function parseHTML(html) {
   let state = data;
-  for(let c of html) {
+  for (let c of html) {
     state = state(c);
   }
   state = state(EOF);
--- week08/NOTE.md
+++ week08/NOTE.md
@@ -1,1 +1,193 @@
 # 每周总结可以写在这里
+
+## CSS选择器
+
+### 1、选择器语法
+
+#### 简单选择器
+
+- div svg | a (namespace?)
+- .class
+- #id
+- [attr=value] （~ | ）（[arrt]）
+- :hover
+- ::before
+
+#### 复合选择器(与的关系)
+
+- <简单选择器><简单选择器><简单选择器>
+- `*` 或者div必须写在最前面
+
+#### 复杂选择器
+
+- <复合选择器><sp><复合选择器>	子孙关系
+- <复合选择器>">"<复合选择器>	父子关系
+- <复合选择器>"~"<复合选择器>	sibling
+- <复合选择器>"+"<复合选择器>	sibling
+- <复合选择器>"||"<复合选择器>	leve4，兼容性不好
+
+#### 选择器列表
+
+- 使用“,”分隔
+
+### 2、选择器优先级
+
+- [inline-element, id, class | pseudo | attr, type] 内联选择器个数，id选择器个数，class选择器/属性选择器个数，标签选择器个数
+- :not() 不参与优先级计算
+
+https://developer.mozilla.org/zh-CN/docs/Web/CSS/Specificity
+https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Cascade_and_inheritance#Specificity_2
+
+```
+写出下面选择器的优先级
+div#a.b.c[id=x]	[0,1,3,1]
+#a:not(#b)		[0,2,0,0]
+*.a				[0,0,1,0]
+div.a			[0,0,1,1]
+```
+
+- 伪类
+  - 链接/行为
+    - :any-link
+    - :link      :visited
+    - :hover
+    - :active
+    - :focus
+    - :target
+  - 树结构
+    - :empty           没有子元素
+    - :nth-child()
+    - :nth-last-child()
+    - :first-child :last-child :only-child
+  - 逻辑型
+    - :not伪类
+    - :where      :has
+- 伪元素
+  - ::before
+  - ::after
+  - ::first-line   选中已有内容的第一行（第一行与回车没有关系，只是显示）
+    - font系列
+    - color系列
+    - background系列
+    - word-spacing
+    - letter-spacing
+    - text-decorating
+    - text-transform
+    - line-height
+  - ::first-letter
+    - font系列
+    - color系列
+    - background系列
+    - word-spacing
+    - letter-spacing
+    - text-decorating
+    - text-transform
+    - line-height
+    - float
+    - vertical-align
+    - 盒模型系列：margin、padding、border
+
+```
+为什么first-letter可以设置display:float之类的，而first-line不行？
+选中的first-line如果加上float属性会脱离文档流，font属性作用于文字而非box上。
+```
+
+作业：编写一个match函数
+
+function match(selector, element){
+
+​	return true;
+
+}
+
+match("div #id.class", document.getElementById("id"));
+
+## CSS排版
+
+### 1、盒（Box）
+
+**标签**  **元素**  **盒**
+
+CSS选择器选中的时 **元素**
+
+CSS选择器选中的 **元素**，在排版时可能会产生多个盒
+
+排版盒渲染的基本单位时盒
+
+### 2、盒模型
+
+- content
+- padding
+- border
+- margin
+
+box-sizing
+
+- content-box
+- border-box = content-box + border + padding
+
+### 3、正常流（Normal Flow）
+
+#### 从写字的角度考虑排版
+- 从左到右书写
+- 同一行写的文字都是对齐的
+- 一行写满了，就换到下一行
+
+#### 正常流排版
+- 收集盒进行
+- 计算盒子行中的排布
+- 计算行的排布
+
+#### 排版元素
+- 正常文字
+- inline-box元素，有自己的宽盒高属性（IFC）
+- block-box元素，占一整行 （BFC: Block formatting context）
+
+### 4、正常流的行模型
+
+- baseline基线
+- 一个inline-box里面没有内容，它的基线在盒子底部
+- inline-box元素本身的height可能会超过line-height，vertical-align属性就会保证行内最高的元素对齐方式正确
+- 建议：inline-box元素的vertical-laign属性只使用：top、middle、bottom
+
+  ```
+  获取行盒
+  $0.getClientRects();
+  ```
+
+### 5、float与clear
+
+- clear属性可以用于清除浮动
+- float元素可以添加clear属性达到换行的目的
+- float元素也可以添加margin属性
+- inline-block元素之间会有空白，来自于html标签间的换行符
+
+### 6、BFC
+
+```
+Floats, absolutely positioned elements, block containers (such as inline-blocks, table-cells, and table-captions) that are not block boxes, and block boxes with 'overflow' other than 'visible' (except when that value has been propagated to the viewport) establish new block formatting contexts for their contents.
+
+Block-level boxes that are also block containers are called block boxes.
+```
+
+https://w3.org/TR/2011/REC-CSS2-20110607/visuren.html#block-formatting
+
+- 对边距留白margin的影响
+- 正常流的margin可以复用
+- 正常流中间嵌套一个新元素
+  - 容器触发条件：
+    - overflow:hidden（overflow:visible无影响，称之为BFC的合并）
+    - display:inline-block
+    - margin-top的效果
+    - 其他正常流元素
+- BFC 与float之间相互作用
+
+### 7、Flex排版
+
+- 收集盒进行
+- 计算盒再主轴方向的排布
+- 计算盒再交叉轴方向的排布
+- component
+  - flex-grow
+  - flex-basis
+  - flex-shrink
